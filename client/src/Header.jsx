30ee74764ed68e25cddf2ad8294949a82ee35f78
Anton Ekblad <anton@ekblad.cc>
2020-01-10 11:15:16 +0100

    Add header component.

---
+++ client/src/Header.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { googleSignOut } from './GoogleSignIn.jsx';
+import './css/Header.css';
+
+function Header(props) {
+    const signOut = () => {
+        googleSignOut(() => {
+            if(props.onSignOut) {
+                props.onSignOut();
+            }
+        });
+    };
+    return (
+        <menu type="toolbar" className="topMenu">
+            <li>
+                <Link to="/">Home</Link>
+            </li>
+            {props.user &&
+             <li>
+                <button onClick={signOut}>Sign out</button>
+             </li>
+            }
+            {props.user && props.user.isAdmin &&
+             <li>
+                 <Link>Admin</Link>
+             </li>
+            }
+        </menu>
+    );
+}
+
+export default Header;
